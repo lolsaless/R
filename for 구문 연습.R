2bfcf4746516aa7cb05efe78f1ece7c111ad20a1
air <hoof.jo@gmail.com>
2020-11-20 10:58:27 +0900

    1

---
+++ for 구문 연습.R
@@ -0,0 +1,46 @@
+fun <- function(x,y) {
+  print(x + y)
+}
+
+
+fun(1,2)
+
+
+
+
+for(i in 1:20) {
+  if (i%%2 == 0) {
+    print(i+1000)
+  } else {
+    print(i)
+  }
+}
+
+print(10, '1')
+
+
+while (i <= 10) {
+  print(i)
+  i <- i+1
+}
+
+
+while(i <= 10) {
+  print(i)
+  i <- i+1
+}
+
+
+for_list <- list()
+
+
+
+for(i in 1:4) {
+  for(ii in 5:6) {
+    
+     
+  }
+  for_list[[i]] <- (ii)
+}
+
+print(for_list)
