8d5dcbf0d8b3dbcda02fdb08fa340a23ea16e243
lolsaless <hoof.jo@gmail.com>
2023-02-07 16:40:49 +0900

    20230207
    zzㅇㄴㄹㅇㅇㅇㅏㅇㅏㅇㅏㅇㅏㄹㅓㄹㅓㅓㄹ

--- 3. book R for everyone/12.dplyr패키지로 데이터 다루기.R
+++ 3. book R for everyone/12.dplyr패키지로 데이터 다루기.R
@@ -10,3 +10,9 @@ theCols
 
 diamonds %>% select(.dots = theCols)
 diamonds %>% select(one_of('carat', 'price'))
+diamonds %>% select(c('carat', 'price'))
+diamonds[, c('carat', 'price')]
+
+diamonds %>% select(starts_with("c"))
+diamonds %>% select(ends_with("t"))
+diamonds %>% select(contains('i'))
