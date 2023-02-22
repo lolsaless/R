702ab9a05b3bfa54a8d7eeb00d145a3e88c7422c
lolsaless <hoof.jo@gmail.com>
2023-02-22 18:20:10 +0900

    111

---
+++ Book/R 데이터 분석 입문/데이터타입.R
@@ -0,0 +1,34 @@
+x <- c(1,2,3, "a", 2,3,4,0)
+x
+class(x)
+b <- c(1,2,3,4)
+b
+class(b)
+
+b
+
+c <- c(1L, 2L, 3L, 4L)
+
+b + c
+
+
+class(b + c)
+
+seq(2,3,1, length.out = 19)
+seq(1,3,1, length = 19)
+
+
+c <- c("hello", "world")
+d <- c(FALSE, TRUE)
+e <- as.factor(c)
+
+f <- c(d, e)
+print(f)
+class(f)
+
+class(f[1])
+
+7 %/% 2
+7 %% 2
+
+#리스트
