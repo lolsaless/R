92dfd64a902c1ed63b4768c1769d07d27980b30d
lolsaless <hoof.jo@gmail.com>
2023-04-04 17:29:43 +0900

    20240404

--- Data crawling/식품안전나라 크롤링/20230403_식품안전나라.R
+++ Data crawling/식품안전나라 크롤링/20230403_식품안전나라.R
@@ -27,7 +27,7 @@ Search_bar$setElementAttribute("value", "혼합음료")
 Search_btn$clickElement()
 
 #for 반복문
-for (page_num in 1:623) {
+for (page_num in 25:613) {
     
     id <- remDr$getPageSource()[[1]] %>% 
         read_html() %>% 
@@ -38,35 +38,39 @@ for (page_num in 1:623) {
     
     df_temp <- list()
     
-    for (i in 1:length(id)) {
-        id_xpath <- paste0("//*[@id=", id[i], "]")
-        food_data <- remDr$findElement(using = "xpath", value = id_xpath)
+    tryCatch({
+        for (i in 1:length(id)) {
+            id_xpath <- paste0("//*[@id=", id[i], "]")
+            food_data <- remDr$findElement(using = "xpath", value = id_xpath)
+            Sys.sleep(3)
+            food_data$clickElement()
+            Sys.sleep(3)
+            
+            raw_data <- remDr$getPageSource()[[1]] %>% read_html() %>% html_table(fill = TRUE)
+            
+            temp_list <- c(raw_data[[2]], raw_data[[6]])
+            temp <- t(unlist(temp_list))
+            temp <- as.data.frame(temp)
+            
+            df_temp[[i]] <- temp
+            
+            click_close <- remDr$findElement(using = "xpath", value = '//*[@id="close"]')
+            click_close$clickElement()
+            
+            Sys.sleep(3)
+        }
+        
+        df_raw_data <- as.data.frame(raw_data[[1]])
+        df_data <- do.call(bind_rows, df_temp)
+        df_data <- bind_cols(df_raw_data, df_data)
+        write.csv(df_data, paste0('drink_page_', page_num, '.csv'), fileEncoding = "UTF-8")
         Sys.sleep(3)
-        food_data$clickElement()
-        Sys.sleep(3)
         
-        raw_data <- remDr$getPageSource()[[1]] %>% read_html() %>% html_table(fill = TRUE)
-        
-        raw_data[[2]]
-        raw_data[[6]]
-        
-        temp_list <- c(raw_data[[2]], raw_data[[6]])
-        temp <- t(unlist(temp_list))
-        temp <- as.data.frame(temp)
-        
-        df_temp[[i]] <- temp
-        
-        click_close <- remDr$findElement(using = "xpath", value = '//*[@id="close"]')
-        click_close$clickElement()
-        
-        Sys.sleep(3)
-    }
-    
-    df_data <- do.call(bind_rows, df_temp)
-    write.csv(df_data, paste0('drink_page', page_num, '.csv'))
-    Sys.sleep(3)
-    
-    click_next <- remDr$findElement(using = "xpath", value = '//*[@id="contents"]/main/section/div[2]/div[3]/div/ul/li[7]/a')
-    click_next$clickElement()
-    Sys.sleep(35)
+        click_next <- remDr$findElement(using = "xpath", value = '//*[@id="contents"]/main/section/div[2]/div[3]/div/ul/li[7]/a')
+        click_next$clickElement()
+        Sys.sleep(40)
+    }, error = function(e) {
+        warning(paste0("Error: page_", page_num))
+    })
+
 }
