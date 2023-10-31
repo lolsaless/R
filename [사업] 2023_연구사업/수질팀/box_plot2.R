b0faa7c29b495c4d74cfea3aeeac7eee389a23bf
LOlsaless <hoof.jo@gmail.com>
2023-10-31 17:21:55 +0900

    Update box_plot2.R

--- [사업] 2023_연구사업/수질팀/box_plot2.R
+++ [사업] 2023_연구사업/수질팀/box_plot2.R
@@ -18,7 +18,35 @@ df_long <- df %>%
 
 # Plotting
 ggplot(df_long, aes(group, Value, fill = Type)) +
+    geom_boxplot(aes(fill = group),position = position_dodge(width = 1), alpha = 0.7) +
+    labs(title = "Transformed Log NP and OP Values by Group", x = "Group", y = "Transformed Value") +
+    theme_minimal()
+
+
+# Importing Libraries
+library(readxl)
+library(ggplot2)
+library(tidyverse)
+
+# Dummy data as given
+df <- read_excel("phenol_data.xlsx")
+
+# Data Transformation for NP
+df$Log_NP <- log10(df$NP + 1)
+
+# Data Transformation for OP
+df$Log_OP <- log10(df$OP + 1)
+
+# Changing order of groups
+df$group <- factor(df$group, levels = c("Car wash", "Textile", "Food", "Paper&Leather", "Plastic", "Plating&Metal",
+                                        "Rubber", "Others"))  # 바뀐 부분
+
+# Reshape the dataframe
+df_long <- df %>%
+    gather(Type, Value, Log_NP, Log_OP)
+
+# Plotting
+ggplot(df_long, aes(group, Value, fill = Type)) +
     geom_boxplot(position = position_dodge(width = 1), alpha = 0.7) +
     labs(title = "Transformed Log NP and OP Values by Group", x = "Group", y = "Transformed Value") +
     theme_minimal()
-_
